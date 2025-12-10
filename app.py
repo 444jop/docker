07f129802e682fdfac3d6b9053f4857f8a302c55
MZC01-JJH2248 <mzc01-jjh2248@MZC01-JJH2248.local>
2025-12-10 09:08:19 +0900

    Update app v2.2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # 파드 이름이나 환경변수를 출력해 보면 나중에 로드밸런싱 확인에 좋습니다
-    return f"Hello Kubernetes! This is Python Flask v2.1."
+    return f"Hello Kubernetes! This is Python Flask v2.2."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
